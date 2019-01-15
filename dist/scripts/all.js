98dcc103e678e6e8b5ea848ddcada0c45ec82eca
Viktor Bulvarenko <silken.ua@gmail.com>
2019-01-15 21:07:31 +0200

    start MoCart

--- dist/scripts/all.js
+++
@@ -1,2 +0,0 @@
-"use strict";var rootDiv=document.querySelector(".root"),paragraph=document.createElement("p");paragraph.textContent="JS is wonderfully, create in js file",rootDiv.appendChild(paragraph);
-//# sourceMappingURL=all.js.map
